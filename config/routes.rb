8e86d296c145816a4b7315ffbc2d9609d3f33bbc
Donna Torres <todonnatorres@icloud.com>
2021-01-14 21:40:17 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,23 @@ Rails.application.routes.draw do
   end
 
   resources :posts, only: %i[index show new create edit update]
+  
+  # get '/admin/stats', to: 'stats#index'
+# scope allows to prefix block of routes under one grouping to keep code DRY. groups URLS
 
-  get '/admin/stats', to: 'stats#index'
+  # scope '/admin' do
+  #   resources :stats, only: [:index]
+  # end 
+  
+  #scope changed due to subfolder structure changing in controller and views. 
+  # scope '/admin', module: 'admin' do
+  #   resources :stats, only: [:index]
+  # end
+
+  namespace :admin do
+    resources :stats, only: [:index]
+  end 
+
 
   root 'posts#index'
 end
